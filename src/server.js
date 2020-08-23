91bc0fcb1b9967777207aab5752ae99b7c198eb9
Lucas Vieira <vyeiralucas@gmail.com>
2020-08-23 09:49:07 -0300

    mod

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const routes = require('./routes');
+const PORT = process.env.PORT || 3040;
+const env = process.env.NODE_ENV;
 
 require('./database');
 
@@ -10,4 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT == 3333);
+app.listen(PORT, () => {
+  console.log(`ENV: ${env}`)
+  console.log(`Server is listening on port ${PORT}`);
+});
